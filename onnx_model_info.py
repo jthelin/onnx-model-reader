65b447c59c753c1919a85ac7d556112921151fd7
Jorgen Thelin <jthelin@microsoft.com>
2019-06-20 16:18:48 -0700

    Print model in a more human readable representation.
    Include `polish_model` function.

--- onnx_model_info.py
+++ onnx_model_info.py
@@ -1,23 +1,39 @@
 import onnx
+import onnx.utils
 import sys
 
 from onnx import ModelProto
+from onnx import optimizer
 
 model_file = sys.argv[1]
 
-model = ModelProto()
-
 print("-- Opening ONNX file=%s" % model_file)
-
-f = open(model_file, "rb")
-model.ParseFromString(f.read())
-f.close()
+model: ModelProto = onnx.load(model_file)
 
 print("-- ONNX OpSet=%s" % model.opset_import)
 
 print("-- ONNX model - Number of nodes=%d" % len(model.graph.node))
 
-print("-- ONNX model dump=\n%s" % model)
+print()
+print("-- Begin ONNX model --")
+# Print a human readable representation of the model graph
+print(onnx.helper.printable_graph(model.graph))
+print("-- End ONNX model --")
+print()
 
 onnx.checker.check_model(model)
 print('-- ONNX model validated OK')
+print()
+
+# A full list of supported optimization passes can be found using get_available_passes()
+all_passes = optimizer.get_available_passes()
+print("-- Available optimization passes:")
+for p in all_passes:
+    print(p)
+print()
+
+# Run model checker, optimizer, & shape inference engine on the model, and also strip any doc_string's.
+print("-- Optimizing and polishing ONNX model")
+polished_model = onnx.utils.polish_model(model)
+
+print("-- ONNX polished model - Number of nodes=%d" % len(polished_model.graph.node))
